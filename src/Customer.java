d417a63df35fbb2776c9d5f3afac99ea4c101c6e
aartikansal <60615341+aartikansal@users.noreply.github.com>
2023-02-18 17:39:46 -0500

    Initial commit

---
+++ src/Customer.java
@@ -0,0 +1,18 @@
+public class Customer {
+    //STATE
+    private String name;
+    private Address homeAddress;
+    private Address workAddress;
+
+    //Customer contains reference to an instance of the object address
+    //object composition
+
+    //CREATING constructors
+    public Customer(String name, Address homeAddress){
+        this.name = name;
+        this.homeAddress = homeAddress;
+
+    }
+
+    //operations ---method
+}
---
+++ src/CustomerRunner.java
@@ -0,0 +1,5 @@
+public class CustomerRunner {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
