0aed6eade53361942c94c852ea771295eb60c075
aartikansal <60615341+aartikansal@users.noreply.github.com>
2023-02-18 21:54:59 -0500

    super class Inheritance Is  A relationship Abstract class Methods added

--- src/Inheritance/Recipe1.java
+++ src/Inheritance/Recipe1.java
@@ -1,4 +1,21 @@
 package Inheritance;
 
-public class Recipe1 {
+public class Recipe1 extends AbstractRecipe {
+    @Override
+    void getReady() {
+        System.out.println("Get the raw materials");
+        System.out.println("Get the utensils");
+    }
+
+    @Override
+    void doTheDish() {
+        System.out.println("do the dish");
+
+
+    }
+
+    @Override
+    void cleanup() {
+        System.out.println("cleanup  the utensils");
+    }
 }
